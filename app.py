adf80b23f941258e59b993c21c52a4c1116d9d14
sean <seanmalone1993@gmail.com>
2026-08-26 21:50:09 +0100

    add ability to see user info

--- app.py
+++ app.py
@@ -7,11 +7,17 @@ program_active = True
 
 github_api_url = 'https://api.github.com/users/'
 while(program_active):
-    username_input = input("Please enter the github user for whom you want to see the activity. OR - type exit to exit the program: ")
+    search_type = input("What do you want to do? View user events, or user info? type events or info. OR type exit to stop the program: ")
+    
+    if (search_type.upper() == "EXIT"):
+        program_active = False
+        continue
 
-    if (username_input.upper() != "EXIT"):
+    username_input = input("Please enter the github user. OR - type exit to exit the program: ")
+
+    if (search_type.upper() == "EVENTS"):
         try:
-            req = requests.get(github_api_url + username_input + '/events')
+            req = requests.get(f"https://api.github.com/users/{username_input}/events")
 
             if (req.status_code == '403'):
                 print("403: Forbidden")
@@ -41,8 +47,43 @@ while(program_active):
                 print(f'Event ID: {id} \n Event Type: {type} \n Actor Login: {actor_login} \n Actor URL: {actor_url} \n Repo Name: {repo_name} \n Created At: {created_at} ')      
         except:
             print("could not get github activity")
-    else:
-        program_active = False
+
+        continue
+
+    if (search_type.upper() == "INFO"):
+        try:
+            print(f"{github_api_url}{username_input}")
+            req = requests.get(f"https://api.github.com/users/{username_input}")
+
+            if (req.status_code == '403'):
+                print("403: Forbidden")
+                continue
+
+            if (req.status_code == '503'):
+                print("503: Service unavailable")
+                continue
+
+            if (req.status_code == '304'):
+                print("304: Not modified")
+                continue
+
+            json = req.json()
+             
+            id = json["id"]
+            url = json["url"]
+            user_name = json["name"]
+            user_company = json["company"]
+            user_location = json["location"]
+            user_bio = json["bio"]
+            user_public_repos = json["public_repos"]
+            user_followers = json["followers"]
+
+            created_at = json["created_at"]
+                
+            print(f'User ID: {id} \n URL: {url} \n Username: {user_name} \n Company: {user_company} \n Location: {user_location} \n Bio: {user_bio} \n Public repos: {user_public_repos} \n Followers: {user_followers} \n Created at: {created_at} ')      
+        except:
+            print("could not get github user info")
+
+        continue
 
 
-# TODO options to go back to see another user's activity or end the program
